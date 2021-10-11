3ce5d910b5bdc6025cd9113e975cdac7ce2f284a
farid95g <f95guluzade@gmail.com>
2021-10-11 09:34:02 +0400

    focusing into search input on load of the page

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
-const Search = ({
-  value,
-  onChange,
-  children,
-  onSubmit
-}) => (
-  <form onSubmit={onSubmit}>
-    <div>
-      <label>{children}:</label><br />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <input type="text" value={value} onChange={onChange} />
-        <button type="submit">Search</button>
-      </div>
-    </div>
-  </form>
-);
+class Search extends React.Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
 
+  render() {
+    const {
+      value,
+      onChange,
+      children,
+      onSubmit
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>{children}:</label><br />
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <input
+              type="text"
+              value={value}
+              onChange={onChange}
+              ref={node => this.input = node}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
 export default Search;
